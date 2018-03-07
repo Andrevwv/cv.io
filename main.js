176137c20de5edd9a69c78df129dfd6e7bbe508e
andrevwv <andrevwv@gmail.com>
2018-03-07 13:07:25 +0200

    changed content appearance

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 (function () {
   var mainBlockSections = document.querySelectorAll('.content > section');
-  var mainMenuItems = document.querySelectorAll('ul.menu li a');
+  var mainMenuItems = document.querySelectorAll('.menu__button');
+  var mainBlock = document.querySelector('.main__block');
 
     /**
      * Adding class disabled  to all child nodes of array
@@ -25,19 +26,26 @@
       }
   };
 
+  var setMainBlockHeight = function (activeBlock, mainBlockHeight) {
+  mainBlock.height = mainBlockHeight + (activeBlock.offsetHeight - (mainBlockHeight - mainBlock.minHeight))
+  };
+
     /**
      * Function changes information block corresponding to menu item pressed
      */
   var changeSection = function () {
        for (var j = 0; j < mainMenuItems.length; j++) {
           mainMenuItems[j].addEventListener('click', function (evt) {
+              var currentContentBlock = document.querySelector('section.' + evt.currentTarget.dataset.id);
+              var mainBlockCurrentHeight = mainBlock.offsetHeight;
               addDisabledClass(mainBlockSections);
-              document.querySelector('section.' + evt.currentTarget.dataset.id).classList.remove('disabled');
+              currentContentBlock.classList.remove('disabled');
+              setMainBlockHeight(currentContentBlock, mainBlockCurrentHeight);
               removeCurrentItemClass();
               evt.currentTarget.classList.add('current-menu-item');
           });
       }
   };
-
+  mainBlock.height = mainBlock.offsetHeight;
   changeSection();
 })();
