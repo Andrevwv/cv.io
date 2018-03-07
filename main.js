1d8885f8f2694bb0b75ceae81d407b687a4aa564
andrevwv <andrevwv@gmail.com>
2018-03-07 17:57:45 +0200

    added BEM naming

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 (function () {
   var mainBlockSections = document.querySelectorAll('.content > section');
   var mainMenuItems = document.querySelectorAll('.menu__button');
-  var mainBlock = document.querySelector('.main__block');
+  var mainBlock = document.querySelector('.main');
+  var menu = document.querySelector('.menu');
+  var footer = document.querySelector('footer');
 
     /**
      * Adding class disabled  to all child nodes of array
@@ -16,18 +18,26 @@
   };
 
     /**
-     * Removes current-menu-item class from non-active menu item
+     * Removes menu__button--current class from non-active menu item
      */
   var removeCurrentItemClass = function () {
+      var currentMenuItem = null;
       for (var i = 0; i < mainMenuItems.length; i++) {
-          if (mainMenuItems[i].classList.contains('current-menu-item')) {
-              mainMenuItems[i].classList.remove('current-menu-item');
+          if (mainMenuItems[i].classList.contains('menu__button--current')) {
+              currentMenuItem = mainMenuItems[i];
+              mainMenuItems[i].classList.remove('menu__button--current');
           }
       }
+      return currentMenuItem;
   };
 
-  var setMainBlockHeight = function (activeBlock, mainBlockHeight) {
-  mainBlock.height = mainBlockHeight + (activeBlock.offsetHeight - (mainBlockHeight - mainBlock.minHeight))
+  var setMainBlockHeight = function (activeBlock, mainBlockHeight, oldBlock) {
+      if (activeBlock.offsetHeight > 200) {
+          mainBlock.style.height = mainBlockHeight + (activeBlock.offsetHeight - oldBlock.offsetHeight ) + 'px';
+      } else {
+          mainBlock.style.height = activeBlock.offsetHeight + menu.offsetHeight + footer.offsetHeight + 'px';
+
+      }
   };
 
     /**
@@ -36,16 +46,17 @@
   var changeSection = function () {
        for (var j = 0; j < mainMenuItems.length; j++) {
           mainMenuItems[j].addEventListener('click', function (evt) {
-              var currentContentBlock = document.querySelector('section.' + evt.currentTarget.dataset.id);
+              var currentContentBlock = document.querySelector('section.content__' + evt.currentTarget.dataset.id);
               var mainBlockCurrentHeight = mainBlock.offsetHeight;
               addDisabledClass(mainBlockSections);
               currentContentBlock.classList.remove('disabled');
-              setMainBlockHeight(currentContentBlock, mainBlockCurrentHeight);
-              removeCurrentItemClass();
-              evt.currentTarget.classList.add('current-menu-item');
+              var oldBlock = removeCurrentItemClass();
+              // setMainBlockHeight(currentContentBlock, mainBlockCurrentHeight, document.querySelector('section.' + oldBlock.dataset.id));
+              evt.currentTarget.classList.add('menu__button--current');
           });
       }
   };
-  mainBlock.height = mainBlock.offsetHeight;
+  // mainBlock.style.height = mainBlock.offsetHeight + 'px';
+
   changeSection();
 })();
