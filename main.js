cb1c1a5a05da9f9688576963e53d57471a186ac6
andrevwv <andrevwv@gmail.com>
2018-03-13 11:11:04 +0200

    added prefixes

--- main.js
+++ main.js
@@ -64,50 +64,50 @@
       }
   };
 
-  //   /**
-  //    * Changes color scheme of the page, when color-button is clicked
-  //    * @param {event} evt
-  //    */
-  // var colorButtonClickHandler = function (evt) {
-  //     switch (evt.target.dataset.id) {
-  //         case 'red':
-  //             root.style.setProperty('--main-color', '#ce4f4a');
-  //             root.style.setProperty('--selection-color', 'rgba(206, 79, 74, 0.99)');
-  //             background.style.backgroundImage = 'linear-gradient(to bottom right, rgba(1, 160, 228,.8), rgba(255,0,0,0) 40%), linear-gradient(to bottom left, rgba(1, 160, 228,.8), rgba(0,255,0,0) 40%), linear-gradient(to top, rgba(209, 58, 41,.8), rgba(0,0,255,0) 40%)';
-  //             mainPhoto.style.filter = 'grayscale(0)';
-  //             break;
-  //         case 'gray':
-  //             root.style.setProperty('--main-color', '#424242');
-  //             root.style.setProperty('--selection-color', 'rgba(66, 66, 66, 0.99)');
-  //             background.style.backgroundImage = 'linear-gradient(to bottom right, rgba(153, 153, 153,.8), rgba(255,0,0,0) 40%), linear-gradient(to bottom left, rgba(153, 153, 153,.8), rgba(0,255,0,0) 40%), linear-gradient(to top, rgba(22, 22, 22,.8), rgba(0,0,255,0) 40%)';
-  //             mainPhoto.style.filter = 'grayscale(0.9)';
-  //             break;
-  //         case 'blue':
-  //             root.style.setProperty('--main-color', '#2C8BB8');
-  //             root.style.setProperty('--selection-color', 'rgba(0, 125, 188, 0.99)');
-  //             background.style.backgroundImage = 'linear-gradient(to bottom right, rgba(157, 72, 99,.8), rgba(255,0,0,0) 40%), linear-gradient(to bottom left, rgba(157, 72, 99,.8), rgba(0,255,0,0) 40%), linear-gradient(to top, rgba(17, 59, 99,.8), rgba(0,0,255,0) 40%)';
-  //             mainPhoto.style.filter = 'grayscale(0)';
-  //             break;
-  //         case 'green':
-  //             root.style.setProperty('--main-color', '#5fb522');
-  //             root.style.setProperty('--selection-color', 'rgba(66, 188, 0, 0.99)');
-  //             background.style.backgroundImage = 'linear-gradient(to bottom right, rgba(204, 153, 51,.8), rgba(255,0,0,0) 40%), linear-gradient(to bottom left, rgba(204, 153, 51,.8), rgba(0,255,0,0) 40%), linear-gradient(to top, rgba(51, 153, 51,.8), rgba(0,0,255,0) 40%)';
-  //             mainPhoto.style.filter = 'grayscale(0)';
-  //             break;
-  //
-  //     }
-  // };
-  //
-  //   /**
-  //    * Addes handler to theme switch buttons
-  //    */
-  // var addHandlerToThemeButtons = function () {
-  //     for (var i = 0; i < themeButtons.length; i++) {
-  //         themeButtons[i].addEventListener('click', colorButtonClickHandler);
-  //     }
-  // };
+    /**
+     * Changes color scheme of the page, when color-button is clicked
+     * @param {event} evt
+     */
+  var colorButtonClickHandler = function (evt) {
+      switch (evt.target.dataset.id) {
+          case 'red':
+              root.style.setProperty('--main-color', '#ce4f4a');
+              root.style.setProperty('--selection-color', 'rgba(206, 79, 74, 0.99)');
+              background.style.backgroundImage = 'linear-gradient(to bottom right, rgba(1, 160, 228,.8), rgba(255,0,0,0) 40%), linear-gradient(to bottom left, rgba(1, 160, 228,.8), rgba(0,255,0,0) 40%), linear-gradient(to top, rgba(209, 58, 41,.8), rgba(0,0,255,0) 40%)';
+              mainPhoto.style.filter = 'grayscale(0)';
+              break;
+          case 'gray':
+              root.style.setProperty('--main-color', '#424242');
+              root.style.setProperty('--selection-color', 'rgba(66, 66, 66, 0.99)');
+              background.style.backgroundImage = 'linear-gradient(to bottom right, rgba(153, 153, 153,.8), rgba(255,0,0,0) 40%), linear-gradient(to bottom left, rgba(153, 153, 153,.8), rgba(0,255,0,0) 40%), linear-gradient(to top, rgba(22, 22, 22,.8), rgba(0,0,255,0) 40%)';
+              mainPhoto.style.filter = 'grayscale(0.9)';
+              break;
+          case 'blue':
+              root.style.setProperty('--main-color', '#2C8BB8');
+              root.style.setProperty('--selection-color', 'rgba(0, 125, 188, 0.99)');
+              background.style.backgroundImage = 'linear-gradient(to bottom right, rgba(157, 72, 99,.8), rgba(255,0,0,0) 40%), linear-gradient(to bottom left, rgba(157, 72, 99,.8), rgba(0,255,0,0) 40%), linear-gradient(to top, rgba(17, 59, 99,.8), rgba(0,0,255,0) 40%)';
+              mainPhoto.style.filter = 'grayscale(0)';
+              break;
+          case 'green':
+              root.style.setProperty('--main-color', '#5fb522');
+              root.style.setProperty('--selection-color', 'rgba(66, 188, 0, 0.99)');
+              background.style.backgroundImage = 'linear-gradient(to bottom right, rgba(204, 153, 51,.8), rgba(255,0,0,0) 40%), linear-gradient(to bottom left, rgba(204, 153, 51,.8), rgba(0,255,0,0) 40%), linear-gradient(to top, rgba(51, 153, 51,.8), rgba(0,0,255,0) 40%)';
+              mainPhoto.style.filter = 'grayscale(0)';
+              break;
 
-    // addHandlerToThemeButtons();
+      }
+  };
+
+    /**
+     * Addes handler to theme switch buttons
+     */
+  var addHandlerToThemeButtons = function () {
+      for (var i = 0; i < themeButtons.length; i++) {
+          themeButtons[i].addEventListener('click', colorButtonClickHandler);
+      }
+  };
+
+    addHandlerToThemeButtons();
   // mainBlock.style.height = mainBlock.offsetHeight + 'px';
   changeSection();
 })();
